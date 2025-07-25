6e277778346af5849632809da3b3cae0b48da91b
jjown55 <bomeonda99@gmail.com>
2025-07-25 12:41:02 +0900

    리엑트 1일차

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './test';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Test />
+    </div>
+  );
+}
+
+
+export default App;
